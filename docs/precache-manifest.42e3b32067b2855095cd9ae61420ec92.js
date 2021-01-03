0cda220fa244d78af1325f1af4ee1a37888e1545
RonaldoML <rmonserr@espol.edu.ec>
2021-01-03 18:57:03 -0500

    mejora de rutas para producción

---
+++ docs/precache-manifest.42e3b32067b2855095cd9ae61420ec92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7492b4bea347d94d8c3fcdafbb19779c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1056937f1e66ccc3fcf",
+    "url": "/static/js/2.5aaf9860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5aaf9860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f749395920818cfef1",
+    "url": "/static/js/main.7ddff3db.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
